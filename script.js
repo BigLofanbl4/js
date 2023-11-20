784b29346dc40b73e486acae809ea56c797c03f6
BigLofanbl4 <smallofanbl4@gmail.com>
2023-11-20 22:01:18 +0300

    1

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 "use strict";
 
 class Rabbit {
-  
+  constructor(type) {
+    this.type = type;
+  }
+  speak(line) {
+    console.log(`${this.type} rabbit says: "${line}"`);
+  }
 }
+
+Rabbit.prototype.toString = function() {
+  console.log(`${this.type} rabbit`);
+}
+
+let blackRabbit = new Rabbit("Black");
+console.log(String(blackRabbit));
